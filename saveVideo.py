714df86246b3ce04a2ec41d157ae041ca00ac5c3
nicolaseberle <nicolas.eberle.09@gmail.com>
2018-09-07 18:15:42 +0200

    new commit

--- saveVideo.py
+++ saveVideo.py
@@ -3,12 +3,12 @@ import subprocess
 
 
 counter = 0
-fd_out = '/media/pi/02F0-F0FD1/'
+fd_out = '/media/pi/02F0-F0FD1/20180907test/'
 
 while True:
     fout = fd_out + 'fly_tracker_1280_960_' + str(counter) +  '.h264'
     print('save: ' + fout)
-    cmd = 'raspivid -t 60000 -w 1280 -h 960 -fps 30 -ex auto -rf gray -b 1200000 -p 200,200,1280,960 -o ' + fout
+    cmd = 'raspivid -t 3600000 -w 1280 -h 960 -fps 30 -ex auto -rf gray -b 1200000 -p 200,200,1280,960 -o ' + fout
 
     os.system(cmd)
     counter = counter + 1
