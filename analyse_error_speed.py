3674e429ef33acd99579ed26715e605f260ac2e2
Nicolas <nicolas.eberle.09@gmail.com>
2019-08-09 15:19:26 +0200

    analysis of speed error

--- analyse_error_speed.py
+++ analyse_error_speed.py
@@ -8,13 +8,13 @@ from numpy import linalg as LA
 sns.set(style="whitegrid")
 plt.figure()
 V = []
-
+df = []
 for j in range(3,5):
     for i in range(9):
-        measurement = pd.read_csv('./output/fly_arene_'+str(j)+'_num_'+ str(i)+'.csv',delimiter=':')
+        measurement= pd.read_csv('./output/fly_arene_'+str(j)+'_num_'+ str(i)+'.csv',delimiter=':')
         #print(measurement.head())
-
-
+        print('max DT: ',max(measurement['Date(ms)'].diff().shift(-1)))
+        print('min DT: ',min(measurement['Date(ms)'].diff().shift(-1)))
 
         vx = measurement['VX (in pixel/s)']
         vy = measurement['VY (in pixel/s)']
@@ -23,8 +23,10 @@ for j in range(3,5):
         V = np.append(V,v)
         #print(V)
         print('median : ', measurement.describe())
+        df = [df,measurement]
 # Draw a violinplot with a narrower bandwidth than the default
 
+print(df)
 sns.violinplot(data=V, palette="Set3", bw=.2, cut=1, linewidth=1)
 
 # Finalize the figure
