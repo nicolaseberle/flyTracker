2b133212a0d91c3cb6e130bc210a8f46aa5e1bc9
Gert-Jan <gertjanboth@gmail.com>
2020-10-14 21:15:18 +0200

    Removed old code

--- src/flytracker/old/utils/.ipynb_checkpoints/split_movie-checkpoint.sh
+++
@@ -1,6 +0,0 @@
-#ffmpeg -i /Users/gert-janboth/Documents/flyTracker/data/movies/4arenas_noQR.h264 -vf select="between(n\,0\,20)" test.h264
-
-#ffmpeg -i movie.mp4 -an -vf "select=between(n\,250\,750),setpts=PTS-STARTPTS" v_stream.webm
-#ffmpeg -i /Users/gert-janboth/Documents/flyTracker/data/movies/4arenas_noQR.h264 -an -vf "select=between(n\,250\,750),setpts=PTS-STARTPTS" test.h264
-
-ffmpeg -i /Users/gert-janboth/Documents/flyTracker/data/movies/4arenas_QR.h264 -codec copy -map 0 -f segment -segment_list out.csv -segment_frames 100 out%03d.mp4
