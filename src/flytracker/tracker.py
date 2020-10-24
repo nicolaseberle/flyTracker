7ba4116bcd3c3210823f9688f9bde0e2b5cadaad
Gert-Jan <gert-jan.both@cri-paris.org>
2020-10-24 12:18:30 +0000

    Updated tracker with initializer function; now auto finds flies.

--- src/flytracker/tracker.py
+++ src/flytracker/tracker.py
@@ -5,37 +5,85 @@ from sklearn.cluster import KMeans
 
 
 class Tracker:
-    def __init__(self, n_flies, mask):
-        self.n_flies = n_flies
+    def __init__(self, mask, movie_path, output_path):
         self.mask = mask.astype('uint8')
-        self._estimator = KMeans(n_clusters=self.n_flies, n_init=20)
+        self.output_path = output_path
+        self.movie_path = movie_path
 
-    def run(self, path, n_frames):
-        locations = self._localize(path, n_frames)  # Localizing flies
-        dataset = self._post_process(locations)  # Postprocessing
+        self.n_flies = None
 
+    def run(self, n_frames, n_initialize):
+        capture = cv.VideoCapture(self.movie_path)
+        self.n_flies, initial_positions = self.initialize(capture, n_initialize, self.mask)     
+
+        locations = self.localize(capture, self.mask, self.n_flies, initial_positions, n_frames)  # Localizing flies
+        dataset = self.post_process(locations)  # Postprocessing
+
+        #dataset.to_hdf(self.output_path, 'df')
         return dataset
 
-    def _localize(self, path, n_frames):
-        locations = []
+    def initialize(self, capture, n_frames, mask):
+        """
+        Runs a blob detector on the first n_frames and determines the number of flies and builds the estimator.
+        """
+        # Run blob detector at the first n_frames to get number of flies and initial position
+        blob_detector = cv.SimpleBlobDetector_create(self.default_blob_detector_params)
 
-        capture = cv.VideoCapture(path)
-        for frame_idx in np.arange(n_frames):
-            # Load as grayscale, apply mask, find nonzero, all inplace for speed
+        n_blobs = []
+        while True:
             image = cv.cvtColor(capture.read()[1], cv.COLOR_BGR2GRAY)
-            fly_pixels = cv.findNonZero(cv.bitwise_and((image < 120).astype('uint8'), self.mask)).squeeze() 
+            keypoints = blob_detector.detect(image * mask)  # get keypoints
+            n_blobs.append(len(keypoints))
+
+            if len(n_blobs) == n_frames:
+                n_flies = int(np.median(n_blobs)) # we define number of flies as median number found over first n frames
+            if (len(n_blobs) >= n_frames) and (n_blobs[-1] == n_flies):
+                locations = np.array([keypoint.pt for keypoint in keypoints])
+                break
+
+        return n_flies, locations
+
+    def localize(self, capture, mask, n_flies, initial_position, n_frames):
+        locations = [initial_position]
+        estimator = KMeans(n_clusters=n_flies, n_init=1) # we do a lot the first time to make sure we get it right
+        
+        for _ in np.arange(n_frames):
+            # Load as grayscale, apply mask, find nonzero, all inplace for speed,
+            ret, image = capture.read()
+            image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+            fly_pixels = cv.findNonZero(cv.bitwise_and((image < 120).astype('uint8'), mask)).squeeze() 
 
             # Set initial centroids as previous frame' locations and do kmeans
-            if frame_idx >= 1:
-                self._estimator.n_init = 1
-                self._estimator.init = locations[-1]
-            locations.append(self._estimator.fit(fly_pixels).cluster_centers_)
+            estimator.init = locations[-1]
+            locations.append(estimator.fit(fly_pixels).cluster_centers_)
         return locations
 
-    def _post_process(self, locations):
+    def post_process(self, locations):
         n_frames = len(locations)
         n_flies = len(locations[0])
-        identities = (np.arange(n_flies)[None, :] * np.ones((n_frames, n_flies))).reshape(-1, 1)
+        identities = (np.arange(n_flies)[None, :] * np.ones((n_frames, n_flies))).reshape(-1, 1) # we get free tracking from the kmeans
         frames = (np.arange(n_frames)[:, None] * np.ones((n_frames, n_flies))).reshape(-1, 1)
         df = pd.DataFrame(np.concatenate([frames, identities, np.concatenate(locations, axis=0)], axis=1), columns=['frame', 'ID', 'x', 'y'])
         return df
+
+    @property
+    def default_blob_detector_params(self):
+        # Setup SimpleBlobDetector parameters.
+        params = cv.SimpleBlobDetector_Params()
+
+        # Change thresholds
+        params.minThreshold = 20
+        params.maxThreshold = 150
+
+        # Filter by Area.
+        params.filterByArea = True
+        params.minArea = 15
+        params.maxArea = 60
+        params.minDistBetweenBlobs = 1.0
+
+        # Turn off other filters
+        params.filterByCircularity = False
+        params.filterByConvexity = False
+        params.filterByInertia = False
+
+        return params
