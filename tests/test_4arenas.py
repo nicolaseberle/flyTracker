397201cbb7b57e5ff9d0a1141b2e0fc9392ef0e1
Gert-Jan <gert-jan.both@cri-paris.org>
2020-11-07 13:12:44 +0000

    Added test folder

--- tests/test_4arenas.py
+++
@@ -1,6 +0,0 @@
-from flytracker.tracker import Tracker
-from flytracker.utils import FourArenasQRCodeMask
-
-mask = FourArenasQRCodeMask()
-tracker = Tracker(40, mask)
-tracker.run('data/movies/4arenas_QR.h264', 1000)
