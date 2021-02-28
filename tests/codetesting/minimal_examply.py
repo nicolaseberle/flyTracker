257e60681fb21588630f0cbadc88d1ec736be265
Gert-Jan <gert-jan.both@cri-paris.org>
2021-02-28 09:11:49 +0000

    Added timing run.

--- tests/codetesting/minimal_examply.py
+++ tests/codetesting/minimal_examply.py
@@ -1,10 +1,10 @@
-# %% Imports
+import numpy as np
 import cv2 as cv
+
 from flytracker.preprocessing import construct_undistort_map, preprocessing
 from flytracker.tracker import run
-import numpy as np
+from time import time
 
-# %% Mask
 movie_path = "data/testing_data/bruno/seq_1.mp4"
 mapping_folder = "data/distortion_maps/"
 
@@ -16,7 +16,7 @@ image_size = (
 mapping = construct_undistort_map(image_size, mapping_folder)
 image = lambda mask: preprocessing(capture.read()[1], mapping=mapping, mask=mask)
 
-# Creating mask
+
 mask = np.ones(image_size, dtype=bool).T
 mask[:160, :] = 0
 mask[-170:, :] = 0
@@ -28,8 +28,9 @@ mask[:230, -300:] = 0
 mask[-250:, :370] = 0
 mask[830:, 970:] = 0
 
-
-# %% Running
+start = time()
 df = run(movie_path, mask, n_arenas=4, mapping_folder=mapping_folder, n_frames=1000)
+stop = time()
+print(f"{stop - start}")
 df.to_hdf("tests/codetesting/df.hdf", key="df", complevel=9, complib="blosc")
 
