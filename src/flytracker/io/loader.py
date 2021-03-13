4065664e9091a38dd0d6610446654696af14764a
Gert-Jan <gert-jan.both@cri-paris.org>
2021-03-13 12:05:16 +0000

    Changed preprocessor to functional.

--- src/flytracker/io/loader.py
+++ src/flytracker/io/loader.py
@@ -1,11 +1,8 @@
 import cv2 as cv
-from ..preprocessing.undistort_mapping import construct_undistort_map
 from ..preprocessing.preprocessing import preprocessing
 
 
 def opencv_loader(movie_path, mapping_folder, mask):
     capture = cv.VideoCapture(movie_path)
-
-    mapping = construct_undistort_map(mask.shape[::-1], mapping_folder)
-    loader = lambda: preprocessing(capture.read()[1], mapping=mapping, mask=mask)
-    return loader
+    preprocessor = preprocessing(mask, mapping_folder)
+    return lambda: preprocessor(capture.read()[1])
