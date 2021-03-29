6b19bfba47f589ec6365b230f7038651669aa1a5
Gert-Jan <gert-jan.both@cri-paris.org>
2021-03-29 19:48:49 +0000

    Annotating function now uses torch loader.

--- src/flytracker/analysis/annotating.py
+++ src/flytracker/analysis/annotating.py
@@ -1,59 +1,71 @@
 # %%
 import cv2
 import numpy as np
-from os.path import join
+
+from torch.utils.data import DataLoader
+
+from itertools import takewhile
+from ..io import VideoDataset
+from ..preprocessing.undistort_mapping import construct_undistort_map
 
 from scipy.spatial import distance_matrix
 from seaborn import color_palette
-import os
-from itertools import count
 
 
 def annotate(
     df,
-    movie_loc,
+    movie_path,
     mapping_folder,
     output_loc,
     max_frames=None,
     track_length=30,
     touching_distance=10,
+    image_size=(1080, 1280),
 ):
 
+    # Parsing dataframe to numpy array - much faster
     data, n_flies_per_arena = parse_data(df)
+    track_length = int(np.around(track_length * 30))
+    color_fn = lambda ID: color_picker(ID, n_flies_per_arena)
 
+    # Making dataset
     initial_frame = data[0, 0, 0]
+    n_frames = data.shape[0] if max_frames is None else max_frames
+    dataset = VideoDataset(movie_path, preprocessing, mapping_folder, image_size)
     # plus 1 for intiial frame since we plot (n-1, n)
-    loader, image_size = setup_loader(
-        movie_loc, mapping_folder, initial_frame=(initial_frame + 1)
-    )
+    dataset.set_frame(initial_frame + 1)
+
+    # Setting up loader and writer
+    loader = DataLoader(dataset, batch_size=1, pin_memory=True)
     writer = setup_writer(output_loc, image_size, fps=30)
 
-    if max_frames is None:
-        max_frames = 10 ** 9
+    for idx, image in takewhile(lambda x: x[0] <= n_frames, enumerate(loader, start=1)):
+        image = image.numpy().squeeze()
+        lower_frame, upper_frame = np.maximum(idx - track_length, 0), idx
 
-    length = int(np.around(track_length * 30))
-
-    color_fn = lambda ID: color_picker(ID, n_flies_per_arena)
-    # %%
-    for frame in count(start=1):
-        lower_frame, upper_frame = np.maximum(frame - length, 0), frame
-        image = loader()
-        if (image is None) or (frame == (max_frames + 1)):
-            break  # we're finished
-
-        image = add_frame_info(image, f"frame: {upper_frame}")
+        # image = add_frame_info(image, f"frame: {upper_frame}")
         # First write tracks so that numbers don't get occluded.
-        image = write_tracks(image, data[lower_frame:upper_frame], color_fn)
-        image = write_ID(image, data[upper_frame], touching_distance=touching_distance)
+        # image = write_tracks(image, data[lower_frame:upper_frame], color_fn)
+        # image = write_ID(image, data[upper_frame], touching_distance=touching_distance)
         writer.write(image)
 
-        if frame % 1000 == 0:
-            print(f"Done with frame {frame}")
+        if idx % 1000 == 0:
+            print(f"Done with frame {idx}")
     writer.release()
 
     # Compressing to h264 with ffmpeg
-    compressed_loc = output_loc.split(".")[0] + "_compressed.mp4"
-    os.system(f"ffmpeg -i {output_loc} -an -vcodec libx264 -crf 23 {compressed_loc}")
+    # compressed_loc = output_loc.split(".")[0] + "_compressed.mp4"
+    # os.system(f"ffmpeg -i {output_loc} -an -vcodec libx264 -crf 23 {compressed_loc}")
+
+
+def preprocessing(mapping_folder, image_size):
+    def _preprocessing(image) -> np.ndarray:
+        """Preprocesses image to make it ready for kmeans."""
+        processed_image = cv2.remap(image, *mapping, cv2.INTER_LINEAR)
+        return processed_image
+
+    mapping = construct_undistort_map(image_size, mapping_folder)
+    return _preprocessing
 
 
 def parse_data(df):
@@ -71,6 +83,15 @@ def parse_data(df):
     return data, max_n_flies_per_arena
 
 
+def setup_writer(output_loc, image_size, fps=30, color=True):
+    """Returns writer object."""
+    fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+    writer = cv2.VideoWriter(
+        filename=output_loc, fourcc=fourcc, fps=fps, frameSize=image_size, isColor=color
+    )
+    return writer
+
+
 def add_frame_info(img, text):
     """ Add frame number to upper left corner."""
     return cv2.putText(
@@ -82,55 +103,6 @@ def add_frame_info(img, text):
         color=(0, 0, 0),
         thickness=2,
     )
-
-
-def construct_undistort_map(image_size, folder):
-    """ Construct openCV undistort undistort mapping. Make sure files as named below are the supplied folder."""
-    mtx = np.load(join(folder, "mtx_file.npy"))
-    dist = np.load(join(folder, "dist_file.npy"))
-    newcameramtx = np.load(join(folder, "newcameramtx_file.npy"))
-
-    mapping = cv2.initUndistortRectifyMap(mtx, dist, None, newcameramtx, image_size, 5)
-    return mapping
-
-
-def setup_loader(movie_loc, mapping_folder, initial_frame=0):
-    """Returns function which returns preprocessed image when called."""
-
-    def load_fn(capture, mapping):
-        # loads image and applies preprocessing
-        ret, image = capture.read()
-        # image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # cause our video uses bgr
-        if ret is not False:
-            # image = cv2.remap(image, *mapping, cv2.INTER_LINEAR)
-            pass
-        else:
-            image = None
-            capture.release()
-        return image
-
-    # Set up capture
-    cap = cv2.VideoCapture(movie_loc)
-    cap.set(1, initial_frame)  # setting initial frame
-    image_size = (
-        int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),
-        int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)),
-    )
-
-    # Setting up loader
-    mapping = construct_undistort_map(image_size, mapping_folder)
-    loader = lambda: load_fn(cap, mapping)
-
-    return loader, image_size
-
-
-def setup_writer(output_loc, image_size, fps=30, color=True):
-    """Returns writer object."""
-    fourcc = cv2.VideoWriter_fourcc(*"mp4v")
-    writer = cv2.VideoWriter(
-        filename=output_loc, fourcc=fourcc, fps=fps, frameSize=image_size, isColor=color
-    )
-    return writer
 
 
 def write_ID(image, frame_info, touching_distance=10):
