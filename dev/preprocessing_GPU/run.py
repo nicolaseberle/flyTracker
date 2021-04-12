52c423ef3937c43472b1f4e97adbe0baf8d4e30c
Gert-Jan <gert-jan.both@cri-paris.org>
2021-04-12 19:29:20 +0000

    test script comparing parallel and non-parallel.

---
+++ dev/preprocessing_GPU/run.py
@@ -0,0 +1,75 @@
+import torch
+from torch.utils.data import DataLoader
+from flytracker.io.dataset import VideoDataset
+
+from flytracker.tracker import _initialize, _localize
+from flytracker.preprocessing.preprocessing import preprocessing_torch
+from flytracker.localization.blob import localize_blob, default_blob_detector_params
+from flytracker.localization.kmeans import localize_kmeans_torch
+from time import time
+
+movie_path = "data/experiments/bruno/videos/seq_1.mp4"
+n_frames = 5000
+
+mask = torch.ones((1080, 1280), dtype=bool)
+mask[:130, :] = 0
+mask[-160:, :] = 0
+mask[:, :270] = 0
+mask[:, -205:] = 0
+
+mask[:190, :350] = 0
+mask[:195, -270:] = 0
+mask[-220:, :340] = 0
+mask[870:, 1010:] = 0
+
+
+# Running sequential
+start = time()
+dataset = VideoDataset(movie_path, parallel=False)
+loader = DataLoader(dataset, batch_size=None, pin_memory=True)
+
+preprocessor_ini = preprocessing_torch(mask, torch.tensor(255, dtype=torch.uint8))
+initial_position, initial_frame = _initialize(
+    loader, localize_blob, (default_blob_detector_params(), preprocessor_ini), 100
+)
+preprocessor_main = preprocessing_torch(
+    mask.cuda(), torch.tensor(255, dtype=torch.uint8).cuda()
+)
+
+locs_sequential = _localize(
+    loader,
+    localize_kmeans_torch,
+    (preprocessor_main, 120, "cuda"),
+    initial_position,
+    n_frames,
+)
+stop = time()
+print(f"Time for sequential run: {stop - start}s")
+
+# Running parallel
+start = time()
+dataset = VideoDataset(movie_path, parallel=True)
+loader = DataLoader(dataset, batch_size=None, pin_memory=True)
+
+preprocessor_ini = preprocessing_torch(mask, torch.tensor(255, dtype=torch.uint8))
+initial_position, initial_frame = _initialize(
+    loader, localize_blob, (default_blob_detector_params(), preprocessor_ini), 100
+)
+preprocessor_main = preprocessing_torch(
+    mask.cuda(), torch.tensor(255, dtype=torch.uint8).cuda()
+)
+
+locs_parallel = _localize(
+    loader,
+    localize_kmeans_torch,
+    (preprocessor_main, 120, "cuda"),
+    initial_position,
+    n_frames,
+)
+dataset.reader.stop()
+stop = time()
+print(f"Time for parallel run: {stop - start}s")
+print(
+    f"Parallel and sequential give same result: {torch.allclose(torch.stack(locs_parallel), torch.stack(locs_sequential))}"
+)
+
