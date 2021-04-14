9996faa94e53cfc063d1ea2b794a7bf26ee75d56
Gert-Jan Both <32122273+GJBoth@users.noreply.github.com>
2021-04-14 16:05:45 +0200

    Qol functions (#24)
    * Added example folder and helper functions.
    
    * Added helper fns.
    
    * And the notebook.

--- src/flytracker/tracker.py
+++ src/flytracker/tracker.py
@@ -24,17 +24,17 @@ def run(
 ) -> pd.DataFrame:
     """User facing run function with sensible standard settings."""
 
-    loader = DataLoader(movie_path, parallel=parallel, max_queue=20)
-
     if gpu:
         device = "cuda"
         main_localizer = localize_kmeans_torch
         localizer_args = (threshold, 1e-4, device)
     else:
         device = "cpu"
+        parallel = False  # parallel doesnt work on cpu
         main_localizer = localize_kmeans_sklearn
         localizer_args = (threshold, 1e-4)
 
+    loader = DataLoader(movie_path, parallel=parallel, max_queue=20)
     return _run(
         loader,
         preprocessing_blob(mask),
