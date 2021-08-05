13ee0733080524c7ab2fc7644513d0e274e62091
Gert-Jan <gert-jan.both@cri-paris.org>
2021-08-05 13:39:55 +0200

    basic multivideo works.

--- src/flytracker/io/dataset.py
+++ src/flytracker/io/dataset.py
@@ -5,17 +5,13 @@ from .videoreader import VideoReader
 
 
 class DataLoader(torch.utils.data.DataLoader):
-    def __init__(self, path, parallel, **reader_kwargs):
-        self.parallel = parallel
+    def __init__(self, path, **reader_kwargs):
         super().__init__(
-            VideoDataset(path, self.parallel, **reader_kwargs),
-            batch_size=None,
-            pin_memory=True,
+            VideoDataset(path, **reader_kwargs), batch_size=None, pin_memory=True,
         )
 
     def stop(self):
-        if self.parallel:
-            self.dataset.reader.stop()
+        self.dataset.reader.release()
 
     @property
     def frames(self,):
@@ -31,24 +27,18 @@ class DataLoader(torch.utils.data.DataLoader):
 
 
 class VideoDataset(torch.utils.data.IterableDataset):
-    def __init__(self, path, parallel=False, **reader_kwargs):
+    def __init__(self, path, **reader_kwargs):
         super().__init__()
-        self.parallel = parallel
-        if self.parallel:
-            self.reader = VideoReader(path, **reader_kwargs)
-        else:
-            self.reader = cv2.VideoCapture(path, **reader_kwargs)
+        self.reader = cv2.VideoCapture(path, **reader_kwargs)
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        if not self.parallel:
-            frame_idx = int(self.reader.get(cv2.CAP_PROP_POS_FRAMES))
-        else:
-            frame_idx = 0
+        frame_idx = int(self.reader.get(cv2.CAP_PROP_POS_FRAMES))
         succes, image = self.reader.read()
         if succes is False:
+            self.reader.release()
             raise StopIteration
 
         return frame_idx, image
--- src/flytracker/tracker.py
+++ src/flytracker/tracker.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import torch
 from itertools import takewhile
-from typing import Callable, List
+from typing import Callable, Iterable
 
 from .io import DataLoader
 from .preprocessing import preprocessing_blob, preprocessing_kmeans
@@ -10,21 +10,23 @@ from .localization.blob import localize_blob, default_blob_detector_params
 from .localization.kmeans import localize_kmeans_sklearn, localize_kmeans_torch
 from .tracking import tracking
 from .analysis import post_process
+import os
+from natsort import natsorted
 
 
 def run(
-    movie_path: str,
+    path: str,
     mask: torch.Tensor,
     n_arenas: int,
     n_frames: int = np.inf,
     n_ini: int = 100,
     gpu: bool = True,
-    parallel: bool = True,
     threshold: int = 120,
     max_change: float = 20.0,
 ) -> pd.DataFrame:
     """User facing run function with sensible standard settings."""
 
+    # Setting up GPU/CPU settings
     if gpu:
         device = "cuda"
         main_localizer = localize_kmeans_torch
@@ -35,13 +37,24 @@ def run(
         )
     else:
         device = "cpu"
-        parallel = False  # parallel doesnt work on cpu
         main_localizer = localize_kmeans_sklearn
         localizer_args = (torch.tensor(threshold), 1e-4)
 
-    loader = DataLoader(movie_path, parallel=parallel)
-    return _run(
-        loader,
+    # Setting up loaders
+    if os.path.isfile(path) and path[-4:] == ".mp4":
+        files = [path]
+    elif os.path.isdir(path):
+        files = [
+            os.path.join(path, file)
+            for file in filter(lambda file: file[-4:] == ".mp4", os.listdir(path))
+        ]
+    else:
+        raise NotADirectoryError
+    # loaders = [DataLoader(path, parallel=False) for path in natsorted(files)]
+
+    # Running
+    return _multi_run(
+        files,
         preprocessing_blob(mask),
         localize_blob(default_blob_detector_params()),
         preprocessing_kmeans(mask, device=device),
@@ -106,10 +119,20 @@ def _initialize(
             n_flies = int(np.median(n_blobs))
             if n_blobs[-1] == n_flies:
                 break
+    print(enum_idx, frame_idx)
+    pos_array = torch.zeros((loader.frames, n_flies, 3), device=device)
+    pos_array[:, :, -1] = torch.arange(loader.frames)[:, None]  # Adding time
+    pos_array[enum_idx, :, :2] = torch.tensor(locations, dtype=torch.float32)
+    return pos_array
 
+
+def _multi_initialize(
+    loader: DataLoader, init, device,
+):
+    n_flies = init.shape[0]
     pos_array = torch.empty((loader.frames, n_flies, 3), device=device)
     pos_array[:, :, -1] = torch.arange(loader.frames)[:, None]  # Adding time
-    pos_array[frame_idx, :, :2] = torch.tensor(locations, dtype=torch.float32)
+    pos_array[0, :, :2] = torch.tensor(init, dtype=torch.float32)
     return pos_array
 
 
@@ -129,7 +152,7 @@ def _localize(
     ):
         image = image.to(device, non_blocking=True)
         if initializing_frame is None:
-            initializing_frame = frame_idx - 1
+            initializing_frame = np.maximum(frame_idx - 1, 0)
         new_positions, delta_position = localizer(
             preprocessor(image), locations[initializing_frame, :, :2]
         )
@@ -141,3 +164,50 @@ def _localize(
             print(f"Done with frame {frame_idx}")
     loader.stop()
     return locations
+
+
+def _multi_run(
+    files: Iterable[DataLoader],
+    initial_preprocessor: Callable,
+    initial_localizer: Callable,
+    main_preprocessor: Callable,
+    main_localizer: Callable,
+    tracker: Callable,
+    post_process: Callable,
+    n_arenas: int,
+    n_frames: int,
+    n_ini: int,
+    device: str,
+    max_change: float,
+):
+
+    positions_list = []
+    positions = None
+    for file in files:
+        loader = DataLoader(file)
+        if positions is None:
+            positions = _initialize(
+                loader, initial_preprocessor, initial_localizer, n_ini, device
+            )
+        else:
+            positions = _multi_initialize(loader, positions[-1, :, :2], device)
+        # Now run main localizer
+
+        positions = _localize(
+            loader,
+            main_preprocessor,
+            main_localizer,
+            positions,
+            n_frames,
+            device,
+            max_change,
+        )
+        loader.stop()
+        positions_list.append(torch.clone(positions))
+
+    # Turn into dataframe
+    # non_zero_frames = torch.sum(positions[:, :, :2], axis=[1, 2]) != 0
+    # positions = tracker(positions[non_zero_frames])
+    # df_list.append(post_process(positions, n_arenas))
+    # return df_list
+    return positions_list
